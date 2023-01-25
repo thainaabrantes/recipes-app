ee3481f86992f4b11b82190cf5a3435212cef541
Thaina-Abrantes <oliveiraabrantess@gmail.com>
2023-01-25 19:01:34 -0300

    Cria Header e componentes do app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React from 'react';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Meals from './components/Meals';
+import Drinks from './components/Drinks';
+import Profile from './components/Profile';
+import DoneRecipes from './components/DoneRecipes';
+import FavoriteRecipes from './components/FavoriteRecipes';
 
 function App() {
   return (
     <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/meals" component={ Meals } />
+          <Route exact path="/drinks" component={ Drinks } />
+          <Route exact path="/profile" component={ Profile } />
+          <Route exact path="/done-recipes" component={ DoneRecipes } />
+          <Route exact path="/favorite-recipes" component={ FavoriteRecipes } />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
