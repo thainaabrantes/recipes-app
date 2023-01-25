b0e3ce562d7eaee06defc6f5a424460b47ecfe52
Joey Hetfield <joeydefenix@hotmail.com>
2023-01-25 17:21:44 -0300

    Requisito 19

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
+import Recipes from './pages/Recipes';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
+      <Route exact path="/meals" component={ Recipes } />
+      <Route exact path="/drinks" component={ Recipes } />
     </Switch>
   );
 }
