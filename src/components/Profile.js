fcbc2a8d1312b55e4d2285e2572beaeb23f7f666
Mauricio-OC <murisc0@hotmail.com>
2023-01-26 14:49:43 -0300

    Feat: conserta Lint

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
-import Footer from '../components/Footer';
-import Header from '../components/Header';
+import Footer from './Footer';
+import Header from './Header';
 
 function Profile() {
   const history = useHistory();
