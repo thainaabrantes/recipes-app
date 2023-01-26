c641232eac0f05611c479bb47f6e6ae06b3b3dfd
CGC-98 <caiquecorreia.gama@hotmail.com>
2023-01-26 13:06:45 -0300

    feat: history redirect to id;

--- src/context/SearchBarProvider.jsx
+++ src/context/SearchBarProvider.jsx
@@ -14,7 +14,7 @@ import {
 
 function SearchBarProvider({ children }) {
   const [searchBar, setSearchBar] = useState(INIT_SEARCH);
-  const [recipes, setRecipes] = useState({ URL: '', results: [] });
+  const [recipes, setRecipes] = useState({ URL: '', results: [], type: '' });
   const history = useHistory();
   const searchMemo = useMemo(() => ({ searchBar, setSearchBar }), [searchBar]);
 
@@ -22,7 +22,7 @@ function SearchBarProvider({ children }) {
     const { text, radio } = searchBar;
     const { pathname } = history.location;
 
-    function handleLabel(URL) {
+    function handleLabel(URL, type) {
       let setURL;
 
       switch (radio) {
@@ -37,23 +37,23 @@ function SearchBarProvider({ children }) {
         else global.alert(LENGTH_ALERT);
       }
 
-      setRecipes((prev) => ({ ...prev, URL: setURL }));
+      setRecipes((prev) => ({ ...prev, URL: setURL, type }));
       return setURL;
     }
 
     if (text) {
       const actualURL = pathname === '/meals'
-        ? handleLabel(URL_MEALS)
-        : handleLabel(URL_COCKTAILS);
+        ? handleLabel(URL_MEALS, 'meals')
+        : handleLabel(URL_COCKTAILS, 'drinks');
 
       const fetchAPI = async () => {
         try {
           if (actualURL === null) return null;
           const response = await fetch(actualURL);
           const data = await response.json();
-          setRecipes(data);
+          setRecipes((prev) => ({ ...prev, results: data }));
         } catch {
-          setRecipes(null);
+          setRecipes((prev) => ({ ...prev, results: null }));
         }
       }; fetchAPI();
     }
@@ -62,12 +62,15 @@ function SearchBarProvider({ children }) {
   // REDIRECIONAR À TELA DE DETALHES;
   useEffect(() => {
     const { pathname } = history.location;
-    const path = pathname.slice(1);
+    const { results, type } = recipes;
+    const ID = type === 'meals' ? 'idMeal' : 'idDrink';
 
-    if (recipes === null || recipes[path] === null) {
+    if (results === null || results[type] === null) {
       return global.alert(RESULT_ALERT);
-    } if (recipes[path] && recipes[path].length === 1) {
-      return history.push(); // recipes.idMeal
+    } if (results[type] && results[type].length === 1) {
+      return history.push(`${pathname}/${results[type][0][ID]}`); // `${pathname}/${recipes.idMeal}`
+    } if (results[type] && results[type].length > 1) {
+      console.log(results[type]);
     }
   }, [history, recipes]);
 
