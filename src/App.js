e434c018db8a1f6ebb22769d96d14321e8647378
FillipeEduardo <fillipe_oliveira@outlook.com.br>
2023-01-26 12:30:09 -0300

    tela de recipes finalizada

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
+import RecipeDetails from './pages/RecipeDetails';
 import Recipes from './pages/Recipes';
 
 function App() {
@@ -8,6 +9,8 @@ function App() {
       <Route exact path="/" component={ Login } />
       <Route exact path="/meals" component={ Recipes } />
       <Route exact path="/drinks" component={ Recipes } />
+      <Route exact path="/meals/:id" component={ RecipeDetails } />
+      <Route exact path="/drinks/:id" component={ RecipeDetails } />
     </Switch>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
 import PropTypes from 'prop-types';
+import React from 'react';
+import { useHistory } from 'react-router-dom';
 
-function Card({ index, name, image }) {
+function Card({ index, name, image, id }) {
+  const history = useHistory();
+  const handlerClickCard = () => {
+    if (history.location.pathname === '/meals') history.push(`/meals/${id}`);
+    if (history.location.pathname === '/drinks') history.push(`/drinks/${id}`);
+  };
+
   return (
-    <div data-testid={ `${index}-recipe-card` }>
+    <div>
+      <button
+        onClick={ handlerClickCard }
+        data-testid={ `${index}-recipe-card` }
+      >
+        Ver Receita
+      </button>
       <img src={ image } data-testid={ `${index}-card-img` } alt={ name } />
       <p data-testid={ `${index}-card-name` }>{name}</p>
     </div>
@@ -14,6 +27,7 @@ Card.propTypes = {
   index: PropTypes.number.isRequired,
   name: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
 };
 
 export default Card;
