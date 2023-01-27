9a9b0b97f88646728ad3581dee3e1069904fe194
FillipeEduardo <fillipe_oliveira@outlook.com.br>
2023-01-27 17:23:26 -0300

    requisito 40

--- src/hook/useRecipeInProgress.js
+++ src/hook/useRecipeInProgress.js
@@ -1,7 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from 'react';
 import { useLocation, useParams } from 'react-router-dom';
-import { makeFetch, makeListIngredients } from '../services';
+import { checkLocalStorage, makeFetch, makeListIngredients } from '../services';
 
 const NUMBER_SIX = 6;
 
@@ -16,14 +16,59 @@ const useRecipeInProgress = () => {
     if (mealsOrDrink === 'meals') {
       const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
       const data = await makeFetch(url);
-      setIngredients(makeListIngredients(data));
       setRecipe(data.meals[0]);
+      setIngredients(checkLocalStorage(id, makeListIngredients(data), mealsOrDrink));
     } if (mealsOrDrink === 'drink') {
       const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`;
       const data = await makeFetch(url);
       setRecipe(data.drinks[0]);
-      setIngredients(makeListIngredients(data));
+      setIngredients(checkLocalStorage(id, makeListIngredients(data), mealsOrDrink));
     }
+  };
+
+  const setLocalStorage = () => {
+    if (!ingredients) {
+      return;
+    }
+    const listIngredients = ingredients.filter((e) => e.checked).map((j) => j.str);
+    if (localStorage.getItem('inProgressRecipes')) {
+      const local = JSON.parse(localStorage.getItem('inProgressRecipes'));
+      if (mealsOrDrink === 'meals') {
+        local.meals[id] = listIngredients;
+      } if (mealsOrDrink === 'drink') {
+        local.drinks[id] = listIngredients;
+      }
+      localStorage.setItem('inProgressRecipes', JSON.stringify(local));
+      return;
+    }
+    if (mealsOrDrink === 'meals') {
+      const obj = {
+        meals: {
+          [id]: listIngredients,
+        },
+        drinks: {},
+      };
+      localStorage.setItem('inProgressRecipes', JSON.stringify(obj));
+    } if (mealsOrDrink === 'drink') {
+      const obj = {
+        meals: {},
+        drinks: {
+          [id]: listIngredients,
+        },
+      };
+      localStorage.setItem('inProgressRecipes', JSON.stringify(obj));
+    }
+  };
+
+  useEffect(() => {
+    setLocalStorage();
+  }, [ingredients]);
+
+  const handlerClickChecked = ({ target }) => {
+    setIngredients(ingredients.map((e) => {
+      if (target.name === e.str) return { ...e, checked: !e.checked };
+      return e;
+    }));
   };
 
   useEffect(() => {
@@ -33,6 +78,7 @@ const useRecipeInProgress = () => {
   return {
     recipe,
     ingredients,
+    handlerClickChecked,
   };
 };
 
