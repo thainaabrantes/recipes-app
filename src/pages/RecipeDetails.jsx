fd8f62900932e7c6922195ad12b3e3494fe7db40
CGC-98 <caiquecorreia.gama@hotmail.com>
2023-01-27 13:46:37 -0300

    style: remove Recipes2 from /RecipeDetails;

--- src/pages/RecipeDetails.jsx
+++ src/pages/RecipeDetails.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
-import Recipes2 from '../components/Recipes2';
 import useRecipeAPI from '../hook/useRecipeAPi';
 import useRecomendAPI from '../hook/useRecomendAPI';
 
@@ -23,128 +22,125 @@ function RecipeDetails() {
   };
 
   return (
-    <>
-      <div>
-        {
-          location.pathname.includes('/meals')
-            ? (
-              <div>
-                <img
-                  data-testid="recipe-photo"
-                  src={ recipe.strMealThumb }
-                  alt={ recipe.strMeal }
-                />
-                <h1 data-testid="recipe-title">{ recipe.strMeal }</h1>
-                <h4 data-testid="recipe-category">{ recipe.strCategory }</h4>
-                <h4>
-                  Ingredientes:
-                  <ul>
-                    {
-                      ingredientsCalc().map((ingredient, index) => (
-                        <li
-                          data-testid={ `${index}-ingredient-name-and-measure` }
-                          key={ ingredient }
-                        >
-                          { ingredient }
-                        </li>
-                      ))
-                    }
-                  </ul>
-                </h4>
-                <h4> Instruções</h4>
-                <p data-testid="instructions">{ recipe.strInstructions }</p>
-                <iframe
-                  data-testid="video"
-                  title={ recipe.strMeal }
-                  src={ recipe.strYoutube }
-                  frameBorder="0"
-                />
-                {
-                  recomendation === undefined
-                    ? (
-                      <p>Carregando...</p>
-                    )
-                    : (
-                      <div>
-                        {
-                          recomendation.map((recom, index) => (
-                            <div
-                              key={ recom.idDrink }
-                              data-testid={ `${index}-recommendation-card` }
+    <div>
+      {
+        location.pathname.includes('/meals')
+          ? (
+            <div>
+              <img
+                data-testid="recipe-photo"
+                src={ recipe.strMealThumb }
+                alt={ recipe.strMeal }
+              />
+              <h1 data-testid="recipe-title">{ recipe.strMeal }</h1>
+              <h4 data-testid="recipe-category">{ recipe.strCategory }</h4>
+              <h4>
+                Ingredientes:
+                <ul>
+                  {
+                    ingredientsCalc().map((ingredient, index) => (
+                      <li
+                        data-testid={ `${index}-ingredient-name-and-measure` }
+                        key={ ingredient }
+                      >
+                        { ingredient }
+                      </li>
+                    ))
+                  }
+                </ul>
+              </h4>
+              <h4> Instruções</h4>
+              <p data-testid="instructions">{ recipe.strInstructions }</p>
+              <iframe
+                data-testid="video"
+                title={ recipe.strMeal }
+                src={ recipe.strYoutube }
+                frameBorder="0"
+              />
+              {
+                recomendation === undefined
+                  ? (
+                    <p>Carregando...</p>
+                  )
+                  : (
+                    <div>
+                      {
+                        recomendation.map((recom, index) => (
+                          <div
+                            key={ recom.idDrink }
+                            data-testid={ `${index}-recommendation-card` }
+                          >
+                            <img src={ recom.strDrinkThumb } alt={ recom.strDrink } />
+                            <h1
+                              data-testid={ `${index}-recommendation-title` }
                             >
-                              <img src={ recom.strDrinkThumb } alt={ recom.strDrink } />
-                              <h1
-                                data-testid={ `${index}-recommendation-title` }
-                              >
-                                { recom.strDrink }
-                              </h1>
-                            </div>))
-                        }
-                      </div>
-                    )
-                }
-              </div>
-            )
-            : (
-              <div>
-                <img
-                  data-testid="recipe-photo"
-                  src={ recipe.strDrinkThumb }
-                  alt={ recipe.strDrink }
-                />
-                <h1 data-testid="recipe-title">{ recipe.strDrink }</h1>
-                <div data-testid="recipe-category">
-                  { `${recipe.strCategory}: ${recipe.strAlcoholic}` }
+                              { recom.strDrink }
+                            </h1>
+                          </div>))
+                      }
+                    </div>
+                  )
+              }
+            </div>
+          )
+          : (
+            <div>
+              <img
+                data-testid="recipe-photo"
+                src={ recipe.strDrinkThumb }
+                alt={ recipe.strDrink }
+              />
+              <h1 data-testid="recipe-title">{ recipe.strDrink }</h1>
+              <div data-testid="recipe-category">
+                { `${recipe.strCategory}: ${recipe.strAlcoholic}` }
 
-                </div>
-                <h4>
-                  Ingredientes:
-                  <ul>
-                    {
-                      ingredientsCalc().map((ingredient, index) => (
-                        <li
-                          data-testid={ `${index}-ingredient-name-and-measure` }
-                          key={ ingredient }
-                        >
-                          { ingredient }
-                        </li>
-                      ))
-                    }
-                  </ul>
-                </h4>
-                <h4> Instruções</h4>
-                <p data-testid="instructions">{ recipe.strInstructions }</p>
-                {
-                  recomendation === undefined
-                    ? (
-                      <p>Carregando...</p>
-                    )
-                    : (
-                      <div>
-                        {
-                          recomendation.map((recom, index) => (
-                            <div
-                              key={ recom.idMeal }
-                              data-testid={ `${index}-recommendation-card` }
+              </div>
+              <h4>
+                Ingredientes:
+                <ul>
+                  {
+                    ingredientsCalc().map((ingredient, index) => (
+                      <li
+                        data-testid={ `${index}-ingredient-name-and-measure` }
+                        key={ ingredient }
+                      >
+                        { ingredient }
+                      </li>
+                    ))
+                  }
+                </ul>
+              </h4>
+              <h4> Instruções</h4>
+              <p data-testid="instructions">{ recipe.strInstructions }</p>
+              {
+                recomendation === undefined
+                  ? (
+                    <p>Carregando...</p>
+                  )
+                  : (
+                    <div>
+                      {
+                        recomendation.map((recom, index) => (
+                          <div
+                            key={ recom.idMeal }
+                            data-testid={ `${index}-recommendation-card` }
+                          >
+                            <img src={ recom.strMealThumb } alt={ recom.strMeal } />
+                            <h1
+                              data-testid={ `${index}-recommendation-title` }
                             >
-                              <img src={ recom.strMealThumb } alt={ recom.strMeal } />
-                              <h1
-                                data-testid={ `${index}-recommendation-title` }
-                              >
-                                { recom.strMeal }
-                              </h1>
-                            </div>))
-                        }
-                      </div>
-                    )
-                }
-              </div>
-            )
-        }
+                              { recom.strMeal }
+                            </h1>
+                          </div>))
+                      }
+                    </div>
+                  )
+              }
+            </div>
+          )
+      }
 
-      </div>
-      <Recipes2 />
-    </>
+    </div>
   );
 }
 
