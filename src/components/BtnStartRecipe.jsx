0bf4a3845208d7146d65af1f9ff95769b581aaaa
Jonathas Oliveira <jonnutri@gmail.com>
2023-01-30 21:15:19 -0300

    Requisitos 30 e 31 - completos / Restanto apenas Testes da Página

--- src/components/BtnStartRecipe.jsx
+++ src/components/BtnStartRecipe.jsx
@@ -1,23 +1,65 @@
-import React from 'react';
-import { useHistory } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
+import useRecipeAPI from '../hook/useRecipeAPi';
 import '../css/BtnStartRecipe.css';
 
-function BtnStartRecipe(props) {
-  const { id, typeFood } = props;
+function BtnStartRecipe() {
+  const { pageId } = useRecipeAPI();
   const history = useHistory();
+  const [disabled, setDisabled] = useState(false);
+  const [proceed, setProceed] = useState(false);
+  const [food, setFood] = useState('');
+  const location = useLocation();
+  const doneRecipe = JSON.parse(localStorage.getItem('doneRecipes'));
+  const inProgress = JSON.parse(localStorage.getItem('inProgressRecipes'));
 
   const redirectTo = () => {
-    history.push(`/${typeFood}/${id}/in-progress`);
+    history.push(`/${food}/${pageId}/in-progress`);
   };
+
+  const attFood = () => {
+    if (location.pathname.includes('/meals')) {
+      setFood('meals');
+    } else {
+      setFood('drinks');
+    }
+  };
+
+  const isDisabled = () => {
+    if (inProgress !== null && location.pathname.includes('/meals')) {
+      const storage = JSON.parse(localStorage.getItem('inProgressRecipes'));
+      const keys = Object.keys(storage.meals);
+      const find = keys.some((key) => key === pageId.toString());
+      setProceed(find);
+    }
+
+    if (inProgress !== null && location.pathname.includes('/drinks')) {
+      const storage = JSON.parse(localStorage.getItem('inProgressRecipes'));
+      const keys = Object.keys(storage.drinks);
+      const find = keys.some((key) => key === pageId.toString());
+      setProceed(find);
+    }
+
+    if (doneRecipe) {
+      const find = doneRecipe.find((recipe) => recipe.id === id);
+      return setDisabled(find);
+    }
+  };
+
+  useEffect(() => {
+    attFood();
+    isDisabled();
+  }, [proceed]);
 
   return (
     <button
       className="btnStart"
       type="button"
+      disabled={ disabled }
       onClick={ redirectTo }
       data-testid="start-recipe-btn"
     >
-      Start Recipe
+      { !proceed ? 'Start Recipe' : 'Continue Recipe'}
     </button>
   );
 }
