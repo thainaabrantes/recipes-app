de9a09b16499cca37f264ac6d123b73e50a12b0c
CGC-98 <caiquecorreia.gama@hotmail.com>
2023-02-01 10:46:18 -0300

    feat: req56 test completion;

--- src/tests/FavoriteRecipes.test.js
+++ src/tests/FavoriteRecipes.test.js
@@ -1,4 +1,5 @@
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import FavoriteRecipes from '../pages/FavoriteRecipes';
 import renderWithRouter from './helpers/renderWithRouter';
@@ -12,11 +13,11 @@ const FILTER_DRINK = 'filter-by-drink-btn';
 const FIRST_IMG = '0-horizontal-image';
 const FIRST_NAME = '0-horizontal-name';
 const FIRST_TEXT = '0-horizontal-top-text';
-const FIRST_SHARE = '0-horizontal-share-btn';
-const FIRST_FAV = '0-horizontal-favorite-btn';
+const NTH_SHARE = '-horizontal-share-btn';
+const NTH_FAV = '-horizontal-favorite-btn';
 
 const elements = [PROFILE, FILTER_ALL, FILTER_MEAL, FILTER_DRINK,
-  FIRST_IMG, FIRST_NAME, FIRST_TEXT, FIRST_SHARE, FIRST_FAV];
+  FIRST_IMG, FIRST_NAME, FIRST_TEXT, `0${NTH_SHARE}`, `0${NTH_FAV}`];
 
 beforeEach(() => {
   const answer = JSON.stringify(mockFavoriteRecipes);
@@ -33,5 +34,26 @@ describe('Up to 45%:', () => {
     elements.forEach((element) => {
       expect(screen.getByTestId(element)).toBeInTheDocument();
     });
+  });
+
+  test('Remove favorite', () => {
+    const recipesLengthBefore = screen.getAllByRole('listitem');
+    expect(recipesLengthBefore).toHaveLength(3);
+
+    const thirdFav = screen.getByTestId(`0${NTH_FAV}`);
+    userEvent.click(thirdFav);
+
+    const recipesLengthAfter = screen.getAllByRole('listitem');
+    expect(recipesLengthAfter).toHaveLength(2);
+  });
+
+  test('Use filter', () => {
+    const mealFilter = screen.getByTestId(FILTER_MEAL);
+    const ignoredDrink = screen.queryByRole('heading', { level: 2, name: 'Kir' });
+
+    expect(ignoredDrink).toBeInTheDocument();
+
+    userEvent.click(mealFilter);
+    expect(ignoredDrink).not.toBeInTheDocument();
   });
 });
