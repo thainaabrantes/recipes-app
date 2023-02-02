a0fec3b174f23b88878add75c5ca0703c04ce646
FillipeEduardo <fillipe_oliveira@outlook.com.br>
2023-02-02 08:40:11 -0300

    css parcial in-progress

--- src/pages/RecipeInProgress.jsx
+++ src/pages/RecipeInProgress.jsx
@@ -2,8 +2,9 @@ import React, { useEffect, useState } from 'react';
 import '../css/recipeInProgress.css';
 import useRecipeInProgress from '../hook/useRecipeInProgress';
 import blackHeart from '../images/blackHeartIcon.svg';
-import whiteHeart from '../images/whiteHeartIcon.svg';
+import mealicon from '../images/meal-icon.svg';
 import shareIcon from '../images/shareIcon.svg';
+import whiteHeart from '../images/whiteHeartIcon.svg';
 
 function RecipeInProgress() {
   const { recipe, ingredients,
@@ -60,31 +61,47 @@ function RecipeInProgress() {
     <div>
       {recipe && (
         <>
-          <img
-            data-testid="recipe-photo"
-            src={ recipe.strDrinkThumb || recipe.strMealThumb }
-            alt="foto-receita"
-          />
-          <h3 data-testid="recipe-title">{recipe.strDrink || recipe.strMeal}</h3>
-          <button
-            onClick={ handlerClickFavorite }
-            data-testid="share-btn"
-            type="button"
-          >
-            <img src={ shareIcon } alt="share icon" />
-          </button>
+          <div className="primeiro-setor-foto">
+            <img
+              className="img-principal"
+              data-testid="recipe-photo"
+              src={ recipe.strDrinkThumb || recipe.strMealThumb }
+              alt="foto-receita"
+            />
+            <div className="category-in-progress">
+              <img src={ mealicon } alt="icon-cake" />
+              {' '}
+              <span data-testid="recipe-category">{recipe.strCategory}</span>
+            </div>
+            <h3
+              className="recipe-title-progress"
+              data-testid="recipe-title"
+            >
+              {recipe.strDrink || recipe.strMeal}
+            </h3>
+            <div className="botoes-in-progress">
+              <button
+                onClick={ handlerClickFavorite }
+                data-testid="share-btn"
+                type="button"
+              >
+                <img src={ shareIcon } alt="share icon" />
+              </button>
+              <button
+                src={ favorite ? blackHeart : whiteHeart }
+                onClick={ handlerFavorite }
+                data-testid="favorite-btn"
+                type="button"
+              >
+                { favorite ? (<img src={ blackHeart } alt="favoriteicon" />)
+                  : (<img src={ whiteHeart } alt="favoriteicon" />) }
+              </button>
+            </div>
+          </div>
           {alertCopy && <p>Link copied!</p>}
-          <button
-            src={ favorite ? blackHeart : whiteHeart }
-            onClick={ handlerFavorite }
-            data-testid="favorite-btn"
-            type="button"
-          >
-            { favorite ? (<img src={ blackHeart } alt="favoriteicon" />)
-              : (<img src={ whiteHeart } alt="favoriteicon" />) }
-          </button>
-          <h4 data-testid="recipe-category">{recipe.strCategory}</h4>
-          {ingredients
+          <p>Ingredients</p>
+          <div className="ingredients-in-progress">
+            {ingredients
           && ingredients.map((e, i) => (
             <label
               className={ `${e.checked && 'scratched'}` }
@@ -102,6 +119,7 @@ function RecipeInProgress() {
               { e.str }
             </label>
           ))}
+          </div>
           <p data-testid="instructions">{recipe.strInstructions}</p>
           <button
             disabled={ isButtonFinishDisabled() }
