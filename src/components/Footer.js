28190d58143026e98f10aedcbc639867bbeffc4f
Thaina-Abrantes <oliveiraabrantess@gmail.com>
2023-02-03 22:50:18 -0300

    Footer css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import mealIcon from '../images/mealIcon.svg';
-import drinkIcon from '../images/drinkIcon.svg';
+import mealIcon from '../images/meal-icon.svg';
+import drinkIcon from '../images/drink-icon.svg';
 import '../css/Footer.css';
 
 function Footer() {
   return (
     <div className="footer" data-testid="footer">
-      <a href="/meals">
-        <img src={ mealIcon } alt="Meals icon" data-testid="meals-bottom-btn" />
-      </a>
       <a href="/drinks">
         <img src={ drinkIcon } alt="Drinks icon" data-testid="drinks-bottom-btn" />
+      </a>
+      <a href="/meals">
+        <img src={ mealIcon } alt="Meals icon" data-testid="meals-bottom-btn" />
       </a>
     </div>
   );
